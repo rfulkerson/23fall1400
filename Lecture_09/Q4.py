88602b186b1409c95901241d8232d4a522a64fe1
Robert A. Fulkerson <rfulkerson@unomaha.edu>
2023-09-27 11:14:25 -0500

    Adding Lectures 10, 11, and 12

---
+++ Lecture_09/Q4.py
@@ -0,0 +1,24 @@
+# Which of the following expressions is equivalent to
+
+x = int(input())
+y = int(input())
+
+if not((tax_rate > 5) and (sales_amt < 10)):
+    print("Hi")
+else:
+    print("Bye")
+
+
+if (tax_rate <= 5) or (sales_amt >= 10):
+    print("Hi")
+else:
+    print("Bye")
+
+# (y <= 5) or (x >= 10)
+# (y < 5) and (x > 10)
+# (y <= 5) and (x >= 10)
+# (y < 5) or (x > 10)
+
+# not (y <= 5) and not (x >= 10)
+
+
